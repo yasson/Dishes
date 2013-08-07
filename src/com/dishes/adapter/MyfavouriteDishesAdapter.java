30589e6d50e7496faf3da8a81ebcc41d50ab4ed6
YangSen <YangSen@yangsen-pc>
2013-08-07 14:44:18 +0800

    add favormodify

---
+++ src/com/dishes/adapter/MyfavouriteDishesAdapter.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package com.dishes.adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dishes.model.DishInfo;
+import com.dishes.ui.R;
+import com.dishes.util.bitmapfun.util.ImageFetcher;
+
+/**
+ * @author YangSen
+ * 
+ */
+public class MyfavouriteDishesAdapter extends BaseAdapter {
+
+	public class ViewHolder {
+
+		public ImageView imageView;
+		public TextView textView;
+		public CheckBox checkBox;
+
+	}
+
+	private List<DishInfo> infos;
+	private Context context;
+	private LayoutInflater layoutInflater;
+	private ImageFetcher imageFetcher;
+	private boolean MODIFY=false;
+
+	/**
+	 * @param applicationContext
+	 * @param infos
+	 */
+	public MyfavouriteDishesAdapter(Context applicationContext,
+			List<DishInfo> infos) {
+		this.context = applicationContext;
+		this.infos = infos;
+		this.layoutInflater = LayoutInflater.from(applicationContext);
+		imageFetcher = new ImageFetcher(applicationContext, 300);
+	}
+
+	/**
+	 * @param mODIFY
+	 *            the mODIFY to set
+	 */
+	public void setMODIFY(boolean mODIFY) {
+		MODIFY = mODIFY;
+	}
+
+	/**
+	 * @return
+	 */
+	public boolean getMODIFY() {
+		return MODIFY;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.widget.Adapter#getCount()
+	 */
+	@Override
+	public int getCount() {
+		return infos.size();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.widget.Adapter#getItem(int)
+	 */
+	@Override
+	public Object getItem(int position) {
+		return infos.get(position);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.widget.Adapter#getItemId(int)
+	 */
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.widget.Adapter#getView(int, android.view.View,
+	 * android.view.ViewGroup)
+	 */
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder viewHolder;
+		if (convertView == null) {
+			convertView = layoutInflater.inflate(
+					R.layout.adapter_myfavordishes, null);
+			viewHolder = new ViewHolder();
+			viewHolder.imageView = (ImageView) convertView
+					.findViewById(R.id.ivfavorpic);
+			viewHolder.textView = (TextView) convertView
+					.findViewById(R.id.tvfavorname);
+			viewHolder.checkBox = (CheckBox) convertView.findViewById(R.id.cb);
+			convertView.setTag(viewHolder);
+		}
+		viewHolder = (ViewHolder) convertView.getTag();
+		viewHolder.textView.setText(infos.get(position).getDishName());
+		imageFetcher.loadImage(infos.get(position).getDishPic(),
+				viewHolder.imageView);
+
+		viewHolder.checkBox
+				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+					@Override
+					public void onCheckedChanged(CompoundButton buttonView,
+							boolean isChecked) {
+					}
+				});
+		return convertView;
+	}
+
+}
--- src/com/dishes/ui/MyfavouriteDishesUi.java
+++ src/com/dishes/ui/MyfavouriteDishesUi.java
@@ -3,15 +3,34 @@
  */
 package com.dishes.ui;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.Button;
+import android.widget.GridView;
+
+import com.dishes.AppContext;
+import com.dishes.adapter.MyfavouriteDishesAdapter;
+import com.dishes.adapter.MyfavouriteDishesAdapter.ViewHolder;
+import com.dishes.dao.Dao;
+import com.dishes.model.DishInfo;
 import com.dishes.ui.base.BaseActivity;
 
 /**
  * @author YangSen
  * 
  */
-public class MyfavouriteDishesUi extends BaseActivity {
+public class MyfavouriteDishesUi extends BaseActivity implements
+		OnClickListener, OnScrollListener {
+	private Button btn_delete;
+	private GridView gv_favor;
+	private MyfavouriteDishesAdapter adapter;
+	private List<DishInfo> infos;
 
 	/*
 	 * (non-Javadoc)
@@ -20,9 +39,115 @@ public class MyfavouriteDishesUi extends BaseActivity {
 	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_myfavouritedishes);
+		initView();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.dishes.ui.base.BaseActivity#onResume()
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Dao dao = new Dao(getApplicationContext());
+		if (AppContext.favorlist != dao.getFavors()) {
+			infos = AppContext.favorlist = dao.getFavors();
+			adapter.notifyDataSetChanged();
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private void initView() {
+		btn_delete = (Button) findViewById(R.id.btn_deletefavor);
+		btn_delete.setOnClickListener(this);
+		gv_favor = (GridView) findViewById(R.id.gv_myfavor);
+		infos = new ArrayList<DishInfo>();
+		Dao dao = new Dao(getApplicationContext());
+		infos = dao.getFavors();
+		AppContext.favorlist = infos;
+		adapter = new MyfavouriteDishesAdapter(getApplicationContext(), infos);
+		gv_favor.setAdapter(adapter);
+		gv_favor.setOnScrollListener(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_deletefavor:
+			if (adapter.getMODIFY()) {
+				adapter.setMODIFY(false);
+				for (int i = 0; i < gv_favor.getChildCount(); i++) {
+					MyfavouriteDishesAdapter.ViewHolder viewHolder = (ViewHolder) gv_favor
+							.getChildAt(i).getTag();
+					viewHolder.checkBox.setVisibility(View.GONE);
+
+				}
+
+			} else {
+				adapter.setMODIFY(true);
+				for (int i = 0; i < gv_favor.getChildCount(); i++) {
+					MyfavouriteDishesAdapter.ViewHolder viewHolder = (ViewHolder) gv_favor
+							.getChildAt(i).getTag();
+					viewHolder.checkBox.setVisibility(View.VISIBLE);
+
+				}
+
+			}
+
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.widget.AbsListView.OnScrollListener#onScroll(android.widget.
+	 * AbsListView, int, int, int)
+	 */
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem,
+			int visibleItemCount, int totalItemCount) {
+		if (adapter.getMODIFY()) {
+			for (int i = 0; i < gv_favor.getChildCount(); i++) {
+				MyfavouriteDishesAdapter.ViewHolder viewHolder = (ViewHolder) gv_favor
+						.getChildAt(i).getTag();
+				viewHolder.checkBox.setVisibility(View.VISIBLE);
+
+			}
+
+		} else {
+			for (int i = 0; i < gv_favor.getChildCount(); i++) {
+				MyfavouriteDishesAdapter.ViewHolder viewHolder = (ViewHolder) gv_favor
+						.getChildAt(i).getTag();
+				viewHolder.checkBox.setVisibility(View.GONE);
+
+			}
+
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.widget.AbsListView.OnScrollListener#onScrollStateChanged(android
+	 * .widget.AbsListView, int)
+	 */
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
 	}
 
 }
